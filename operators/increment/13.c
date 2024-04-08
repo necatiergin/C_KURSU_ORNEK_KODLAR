68a8265ad8faef3bbcc36048867b586578dc2f46
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-04-08 15:53:27 +0300

    Add files via upload

---
+++ operators/increment/13.c
@@ -0,0 +1,15 @@
+//maximum munch kuralı
+
+#include <stdio.h>
+
+int main()
+{
+	int x = 5;
+	int y = 8;
+
+	int z = x+++y;
+
+	printf("x = % d\n", x); //6
+	printf("y = % d\n", y); //8
+	printf("z = % d\n", z); //13
+}

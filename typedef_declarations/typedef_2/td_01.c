2343128a0f2f272742110f73d0bad2667824da8a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-04-08 13:13:19 +0300

    Update td_01.c

--- typedef_declarations/typedef_2/td_01.c
+++ typedef_declarations/typedef_2/td_01.c
@@ -1,1 +1,20 @@
-soon
+typedef int (*Fptr)(int, int);
+
+int foo(int, int);
+int bar(int, int);
+int baz(int, int);
+
+Fptr func(Fptr);
+//int (*func(int (*)(int, int)))(int, int);
+
+int main()
+{
+	Fptr fp = foo;
+	//int (*fp)(int, int) = foo;
+	
+	Fptr ar[] = { foo, bar, baz };
+	//int (*ar[])(int, int) = {foo, bar, baz};
+	
+	Fptr* fpp = &fp;
+	int (**fpp)(int, int) = &fp;
+}

7cbfc863c6607a0f97964bb11c7c1d0a1281ade0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-04-08 13:05:22 +0300

    Create td_01.c

---
+++ typedef_declarations/typedef_1/td_01.c
@@ -0,0 +1,14 @@
+typedef int Word;
+//Word is an type alias for int
+
+Word foo(Word);
+
+int main()
+{
+	Word x = 0, y = 0;
+	Word ar[10] = { 0 };
+	Word* wptr = &x;
+
+	const Word size = 1000;
+	//...
+} 
